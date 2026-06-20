9fece9b6caa3c051863e8bd84c0d69acfd0edb31
Mark <mark@remarkablemark.org>
2026-06-19 20:49:57 -0400

    refactor(game): move text to header

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -5,6 +5,8 @@ const BUTTON_OFFSET = 320
 
 const UI_TEXT_SIZE = 20
 const UI_LINE_SPACING = 4
+const HEADER_HEIGHT = 112
+const HEADER_COLOR = rgb(74, 50, 56)
 
 scene(SCENE.GAME, () => {
   addGrid()
@@ -17,9 +19,16 @@ scene(SCENE.GAME, () => {
   let isSpinning = false
   let continueButton: ReturnType<typeof addButton> | null = null
 
+  add([rect(width(), HEADER_HEIGHT), pos(0, 0), color(HEADER_COLOR)])
+
   const infoLabel = add([
-    text('', { lineSpacing: UI_LINE_SPACING, size: UI_TEXT_SIZE }),
-    pos(12, 12),
+    text('', {
+      align: 'center',
+      lineSpacing: UI_LINE_SPACING,
+      size: UI_TEXT_SIZE,
+    }),
+    pos(width() / 2, HEADER_HEIGHT / 2),
+    anchor('center'),
   ])
 
   const wheel = addWheel()
