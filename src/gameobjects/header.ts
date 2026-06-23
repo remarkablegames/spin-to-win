376c3ced010d7c7af607fb52fa2ccbd8665ea332
Mark <mark@remarkablemark.org>
2026-06-22 21:15:11 -0400

    refactor(header): move level and round to the same line

--- src/gameobjects/header.ts
+++ src/gameobjects/header.ts
@@ -1,7 +1,7 @@
 import { COLOR } from '../constants'
 import { addTooltip } from './tooltip'
 
-const HEADER_HEIGHT = 160
+const HEADER_HEIGHT = 130
 const TEXT_SIZE = 20
 const LINE_HEIGHT = 28
 const CENTER_X = () => width() / 2
@@ -13,18 +13,17 @@ const PROGRESS_BAR_HEIGHT = 8
 export function addHeader() {
   add([rect(width(), HEADER_HEIGHT), pos(), color(COLOR.BROWN)])
 
-  const levelLabel = add([
-    text('', { align: 'center', size: TEXT_SIZE }),
+  const levelRoundLabel = add([
+    text('', {
+      align: 'center',
+      size: TEXT_SIZE,
+      styles: {
+        level: { color: COLOR.MEDIUM_BROWN },
+        round: { color: COLOR.LIGHT_BROWN },
+      },
+    }),
     pos(CENTER_X(), TOP_Y),
     anchor('top'),
-    color(COLOR.DARK_BROWN),
-  ])
-
-  const roundLabel = add([
-    text('', { align: 'center', size: TEXT_SIZE }),
-    pos(CENTER_X(), TOP_Y + LINE_HEIGHT),
-    anchor('top'),
-    color(COLOR.MEDIUM_BROWN),
   ])
 
   const moneyLabel = add([
@@ -35,7 +34,7 @@ export function addHeader() {
         dim: { color: COLOR.LIGHT_GREEN },
       },
     }),
-    pos(CENTER_X(), TOP_Y + LINE_HEIGHT * 2),
+    pos(CENTER_X(), TOP_Y + LINE_HEIGHT),
     anchor('top'),
     color(COLOR.GREEN),
     area(),
@@ -62,7 +61,7 @@ export function addHeader() {
         gold: { color: COLOR.GOLD },
       },
     }),
-    pos(CENTER_X(), TOP_Y + LINE_HEIGHT * 3),
+    pos(CENTER_X(), TOP_Y + LINE_HEIGHT * 2),
     anchor('top'),
     color(COLOR.LIGHT_BROWN),
     area(),
@@ -83,7 +82,7 @@ export function addHeader() {
 
   add([
     rect(PROGRESS_BAR_WIDTH, PROGRESS_BAR_HEIGHT, { radius: 4 }),
-    pos(CENTER_X(), TOP_Y + LINE_HEIGHT * 4 + 4),
+    pos(CENTER_X(), TOP_Y + LINE_HEIGHT * 3 + 4),
     anchor('top'),
     color(COLOR.MEDIUM_BROWN),
     opacity(0.4),
@@ -91,17 +90,19 @@ export function addHeader() {
 
   const progressBarFill = add([
     rect(0, PROGRESS_BAR_HEIGHT, { radius: 4 }),
-    pos(CENTER_X() - PROGRESS_BAR_WIDTH / 2, TOP_Y + LINE_HEIGHT * 4 + 4),
+    pos(CENTER_X() - PROGRESS_BAR_WIDTH / 2, TOP_Y + LINE_HEIGHT * 3 + 4),
     anchor('topleft'),
     color(COLOR.GOLD),
   ])
 
+  let currentLevel = 0
+
   return {
     setLevel(level: number) {
-      levelLabel.text = `Level ${String(level)}`
+      currentLevel = level
     },
     setRound(current: number, total: number) {
-      roundLabel.text = `Round ${String(current)}/${String(total)}`
+      levelRoundLabel.text = `[level]Level ${String(currentLevel)},[/level] [round]Round ${String(current)}/${String(total)}[/round]`
     },
     setScore(current: number, target: number) {
       scoreLabel.text = `[gold]${String(current)}[/gold]/${String(target)}`
