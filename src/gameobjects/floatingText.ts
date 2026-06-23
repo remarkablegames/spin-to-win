8b51ed2bec8e3b47075161db2264350399c7fa4a
Mark <mark@remarkablemark.org>
2026-06-22 23:24:57 -0400

    refactor(gameobjects): add shadow outline to floatingText

--- src/gameobjects/floatingText.ts
+++ src/gameobjects/floatingText.ts
@@ -4,6 +4,7 @@ import { COLOR } from '../constants'
 
 const TEXT_SIZE = 32
 const FLOAT_DISTANCE = 80
+const SHADOW_OFFSET = 2
 export const FLOATING_TEXT_DURATION = 1
 const FADE_DURATION = 0.5
 
@@ -14,14 +15,22 @@ interface AddFloatingTextOptions {
 }
 
 export function addFloatingText(options: AddFloatingTextOptions) {
+  const shadow = add([
+    text(options.text, { size: TEXT_SIZE, align: 'center' }),
+    pos(options.pos.x + SHADOW_OFFSET, options.pos.y + SHADOW_OFFSET),
+    anchor('center'),
+    color(COLOR.BLACK),
+    opacity(),
+    lifespan(FLOATING_TEXT_DURATION, { fade: FADE_DURATION }),
+  ])
+
   const obj = add([
     text(options.text, { size: TEXT_SIZE, align: 'center' }),
     pos(options.pos),
     anchor('center'),
     color(options.color ?? COLOR.WHITE),
-    opacity(1),
+    opacity(),
     lifespan(FLOATING_TEXT_DURATION, { fade: FADE_DURATION }),
-    z(200),
   ])
 
   tween(
@@ -30,6 +39,7 @@ export function addFloatingText(options: AddFloatingTextOptions) {
     FLOATING_TEXT_DURATION,
     (y: number) => {
       obj.pos.y = y
+      shadow.pos.y = y + SHADOW_OFFSET
     },
     easings.easeOutCubic,
   )
