f8fdbc4a7e89df24b33bf878ff330e79d7811401
Mark <mark@remarkablemark.org>
2026-06-24 22:32:07 -0400

    chore(preload): get scene from querystring

--- src/scenes/preload.ts
+++ src/scenes/preload.ts
@@ -1,5 +1,57 @@
 import { MUSIC, SCENE, SOUND, SPRITE } from '../constants'
 
+function parseIntParam(params: URLSearchParams, key: string): number {
+  const value = params.get(key)
+  return value !== null ? parseInt(value, 10) : 0
+}
+
+function getScene(params: URLSearchParams): () => void {
+  const scene = params.get('scene')
+
+  const baseState = {
+    levelIndex: parseIntParam(params, 'level'),
+    levelScore: parseIntParam(params, 'score'),
+    money: parseIntParam(params, 'money'),
+    baseSpins: parseIntParam(params, 'baseSpins') || undefined,
+    passiveIncome: parseIntParam(params, 'passiveIncome') || undefined,
+  }
+
+  switch (scene) {
+    case SCENE.GAME:
+      return () => {
+        go(SCENE.GAME, {
+          ...baseState,
+          roundIndex: parseIntParam(params, 'round'),
+          extraSpins: parseIntParam(params, 'extraSpins') || undefined,
+        })
+      }
+
+    case SCENE.SHOP:
+      return () => {
+        go(SCENE.SHOP, {
+          ...baseState,
+          passiveIncome: parseIntParam(params, 'passiveIncome'),
+          roundIndex: parseIntParam(params, 'round'),
+          artifacts: [],
+          segments: [],
+        })
+      }
+
+    case SCENE.END:
+      return () => {
+        go(SCENE.END, {
+          ...baseState,
+          extraSpins: parseIntParam(params, 'extraSpins') || undefined,
+        })
+      }
+
+    default:
+      return () => {
+        go(SCENE.TITLE)
+      }
+  }
+}
+
 scene(SCENE.PRELOAD, () => {
   const font = loadFont('RobotoMono', '/fonts/RobotoMono.ttf')
 
@@ -12,6 +64,6 @@ scene(SCENE.PRELOAD, () => {
   Object.values(MUSIC).forEach((music) => void loadSound(music.id, music.src))
 
   void font.then(() => {
-    go(SCENE.TITLE)
+    getScene(new URLSearchParams(location.search))()
   })
 })
